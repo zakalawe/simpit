62565aeb21c350a0d1e83570a2cd51d5f4d9301f
James Turner <zakalawe@mac.com>
2018-07-27 18:59:22 +0100

    Add helper script to start local canvas

---
+++ run_canvas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FGFS_PREFIX=/home/jmt/FGFS/dist
+QT_SDK=/home/jmt/QtSDK/5.9.6/gcc_64
+
+export LD_LIBRARY_PATH=$QT_SDK/lib/:$FGFS_PREFIX/lib
+export PATH=$FGFS_PREFIX/bin:$PATH
+
+fgqcanvas 738_captain.json
