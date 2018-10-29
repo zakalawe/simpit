59138bc497086c1c2c13e8806ad10a241a80a41d
James Turner <zakalawe@mac.com>
2018-10-29 22:02:13 +0000

    update launch commands

--- run_738_with_canvas.sh
+++ run_738_with_canvas.sh
@@ -6,19 +6,17 @@ QT_SDK=/home/jmt/QtSDK/5.9.6/gcc_64
 export LD_LIBRARY_PATH=$QT_SDK/lib/:$FGFS_PREFIX/lib
 export PATH=$FGFS_PREFIX/bin:$PATH
 
-#./dist/bin/fgqcanvas $HOME/simpit/738_captain.json &
-
-fgfs --airport=EGPH \
+fgfs --airport=EHLE \
 	--addon=/home/jmt/simpit/simpit-addon \
-	--timeofday=morning \
 	--log-level=info \
 	--prop:/sim/rendering/graphics-window-qt=true \
-	--fg-aircraft=/home/jmt/FGFS/aircraft \
+	--aircraft-dir=/home/jmt/simpit/737-800 \
 	--aircraft=738 \
 	--telnet=5501 \
 	--httpd=8080 \
 	--prop:/sim/rendering/shaders/skydome=true \
-	--prop:/sim/rendering/draw-mask/aircraft=true
+	--prop:/sim/rendering/multithreading-mode=CullDrawThreadPerContext
+	
 
 
 
