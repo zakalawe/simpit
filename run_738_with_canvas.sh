0032b830b6a0894d0340fc3a4834ec8808e81388
James Turner <james@flightgear.org>
2020-03-22 16:00:34 +0000

    Tweaked run/desktop files to use $HOME

--- run_738_with_canvas.sh
+++ run_738_with_canvas.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-FGFS_PREFIX=/home/jmt/FGFS/dist
+FGFS_PREFIX=$HOME/FGFS/dist
 
 export LD_LIBRARY_PATH=$QT_SDK/lib/:$FGFS_PREFIX/lib:$FGFS_PREFIX/lib64
 export PATH=$FGFS_PREFIX/bin:$PATH
 
 fgfs --airport=EGPH \
-	--addon=/home/jmt/simpit/simpit-addon \
+	--addon=$HOME/simpit/simpit-addon \
 	--log-level=info \
-	--fg-scenery=/home/jmt/navigraph_201813_procedures \
+	--fg-scenery=$HOME/navigraph_201813_procedures \
 	--prop:/sim/rendering/graphics-window-qt=true \
-	--aircraft-dir=/home/jmt/simpit/737-800 \
+	--aircraft-dir=$HOME/simpit/737-800 \
 	--aircraft=738 \
 	--telnet=5501 \
 	--httpd=8080 \
 	--prop:/sim/rendering/shaders/skydome=true \
 	--prop:/sim/rendering/multithreading-mode=CullDrawThreadPerContext \
 	--enable-terrasync
+	
 	
 	
 	
--- run_canvas.sh
+++ run_canvas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FGFS_PREFIX=/home/jmt/FGFS/dist
+FGFS_PREFIX=$HOME/FGFS/dist
 
 export LD_LIBRARY_PATH=$QT_SDK/lib/:$FGFS_PREFIX/lib
 export PATH=$FGFS_PREFIX/bin:$PATH
